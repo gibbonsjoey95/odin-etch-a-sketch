1c92009c3728b4512d2fe46a56ed5d9044f13fab
Joey <gibbonsjoey95@gmail.com>
2024-01-21 00:39:29 -0600

    added button to change grid size

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 const container = document.querySelector('.container')
+const changeGridSize = document.querySelector('#changeGridSize')
 
-for(let i = 0; i < 16; i++){
-    let row = document.createElement('div')
+// for(let i = 0; i < 32; i++){
+//     let row = document.createElement('div')
 
-    for(let j = 0; j < 16; j++){
-        let column = document.createElement('div')
+//     for(let j = 0; j < 32; j++){
+//         let column = document.createElement('div')
 
-        column.className = 'column'
+//         column.className = 'column'
 
-        row.append(column)
-    }
+//         column.addEventListener('mouseenter', (event) => {
+//             column.classList.add('active')
+//         })
 
-    row.className = 'row'
+//         row.append(column)
+//     }
 
-    container.appendChild(row)
+//     row.className = 'row'
+
+//     container.appendChild(row)
+// }
+
+const setGridSize = (size) => {
+    for(let i = 0; i < 32; i++){
+            let row = document.createElement('div')
+        
+            for(let j = 0; j < 32; j++){
+                let column = document.createElement('div')
+        
+                column.className = 'column'
+        
+                column.addEventListener('mouseenter', (event) => {
+                    column.classList.add('active')
+                })
+        
+                row.append(column)
+            }
+        
+            row.className = 'row'
+        
+            container.appendChild(row)
+        }
 }
+
+changeGridSize.addEventListener('click', () => {
+    setGridSize(prompt('How big would you like your grid?'))
+})
